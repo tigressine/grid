1c55cf643a5653b3e9a602f6408e1cd69ef894e9
Tiger Sachse <tiger@sachse.io>
2018-07-03 23:01:30 -0400

    updated demo

--- source/demonstration.py
+++ source/demonstration.py
@@ -1,21 +1,31 @@
 import grid
 
-myGrid = grid.Grid()
+example = grid.Grid()
 
 # Assign items using a [y, x] indexing scheme.
-myGrid[0, 0] = "hello"
-myGrid[0, 1] = "world"
+example[0, 0] = 'hello'
+example[0, 1] = 'world'
 
-# Assign entire rows
-myGrid[1] = [x for x in range(5)]
-myGrid[2] = 3.1415926
+# Assign entire rows.
+example[1] = [x for x in range(5)]
+example[2] = 3.1415926
+example[3] = ('a', 'b', 'c')
 
 # Assign really far out of bounds because why not?
-myGrid[4, 5] = True
+example[1, 6] = True
 
 # Even assign backwards!
-myGrid[-1, -3] = False
-myGrid[-2, 0] = "crazy right?"
+example[-1, -3] = False
+example[-2, 1] = 'crazy right?'
 
-print(myGrid)
-print("\nSize is {0}".format(len(myGrid)))
+print(example)
+print('\nSize is {0}'.format(len(example)))
+print('Capacity is {0}'.format(example.capacity()))
+
+# Removing stuff works just fine too.
+example[1, 2] = None
+example[-1] = []
+
+print(example)
+print('\nSize is {0}'.format(len(example)))
+print('Capacity is {0}'.format(example.capacity()))
